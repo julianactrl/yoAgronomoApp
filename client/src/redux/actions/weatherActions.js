35ed8fb9b3dae2fb63f88c5d59f184a59010569f
Dario Rodriguez <69946404+Dariorod1@users.noreply.github.com>
2021-06-10 13:37:14 -0300

    cambios-pronostico

--- client/src/redux/actions/weatherActions.js
+++ client/src/redux/actions/weatherActions.js
@@ -5,7 +5,7 @@ import {
 export const getWeather = (city) => {
     return function(dispatch) {
         axios
-            .get(`http://api.weatherapi.com/v1/forecast.json?key=b8da19009ae34d92b1112005211006&q=${city}&days=3&aqi=no&alerts=no`)
+            .get(`http://api.weatherapi.com/v1/forecast.json?key=b8da19009ae34d92b1112005211006&q=${city}&days=7&aqi=no&alerts=no`)
             .then((r) => r.data)
             .then((data) => {
                 dispatch({
