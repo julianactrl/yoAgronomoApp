a6b6cc0e41e683a541fc4eabefd6d0a54586ad18
julianactrl <juliana.mg3@gmail.com>
2021-06-10 12:34:08 -0300

    register and login

--- api/src/middleware/customMiddleware.js
+++ api/src/middleware/customMiddleware.js
@@ -1,12 +1,14 @@
 function isAuthenticated(req, res, next) {
-    if (req.user) next();
-    else res.sendStatus(401);
-  }
-  
- 
-  
-  module.exports = {
-    isAuthenticated,
-   
-  };
-  
+	if (req.user) next();
+	else res.sendStatus(401)
+};
+
+function isAdmin(req, res, next) {
+	if (req.user.is_admin) next();
+	else res.sendStatus(403)
+};
+
+module.exports = {
+	isAuthenticated,
+	isAdmin
+};
