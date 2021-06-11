133cf6b0174ce8da876eb76c3b41584fd72d7173
selebruno <selebruno@hotmail.com>
2021-06-11 16:50:11 -0300

    introPage

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -4,14 +4,16 @@ import LandingPage from './components/LandingPage/index.js';
 import Weather from './components/Weather/Weather.jsx';
 import NewEmpresa from './components/NewEmpresa/NewEmpresa.js';
 import DetailEmpresa from './components/DetailEmpresa/DetailEmpresa.js';
-import Login from "./components/Auth/Login.jsx";
-import Register from "./components/Auth/Register.jsx";
+import IntroPage from "./components/IntroPage/IntroPage.jsx";
 import News from './components/News/News.js';
 
 
 export default function Routes() {
   return (
     <Switch>
+      <Route exact path="/">
+        <IntroPage/>
+      </Route>
       <Route path="/index">
         <LandingPage/>
       </Route>
