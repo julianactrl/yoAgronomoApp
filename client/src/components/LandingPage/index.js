ca504e2dfdfbf4d56dbbc09eefc5914fd41f3bd4
gonzalo12348 <gonzaloarancibia48@gmail.com>
2021-06-11 16:12:40 -0300

    BtnGoogle conectado a back (solo falta conectar el login)

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -10,26 +10,26 @@ import leaf from './leaf.png'
 const LandingPage = () => {
 
     return (
-        <div className={styles.div}>
-            <div className={styles.title}>
+      <div className={styles.div}>
+          <div className={styles.title}>
             <h1>YO</h1>
             <h2>AGRONOMO</h2>
             <img className={styles.logo} src={leaf}/>
-            </div>
-            <div className={styles.options}>
-          <Link to="/index">
-            <h1 className={styles.login}>Login</h1>
-          </Link>
+          </div>
+          <div className={styles.options}>
+            <Link to="/index">
+              <h1 className={styles.login}>Login</h1>
+            </Link>
 
-          <Link to="/index/register">
-            <h1 className={styles.registro}>Registro</h1>
-          </Link>
-        </div>
+            <Link to="/index/register">
+              <h1 className={styles.registro}>Registro</h1>
+            </Link>
+          </div>
 
-        <Route exact path="/index/register" component={Register} />
-        <Route exact path="/index" component={Login} />
-            <GoogleBtn/>
-        </div>
+          <Route exact path="/index/register" component={Register} />
+          <Route exact path="/index" component={Login} />
+          <GoogleBtn/>
+      </div>
     )
 }
 
