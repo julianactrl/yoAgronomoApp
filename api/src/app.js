4d83fd6245511607db37b641bc04cc51577d2c4a
selebruno <selebruno@hotmail.com>
2021-06-13 16:02:30 -0300

    app merge

--- api/src/app.js
+++ api/src/app.js
@@ -25,15 +25,7 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
-<<<<<<< HEAD
 server.use(passport.initialize());
-=======
-server.use(express.static(path.join(__dirname, 'public')));
-server.use(passport.initialize());
-server.use(passport.session());
-
-
->>>>>>> dev
 
 
 
