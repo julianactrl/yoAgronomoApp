b0f141f67911339f46cb7958be9f300d4f227495
MauriDiPietro <dipietro.jm@gmail.com>
2021-06-14 08:31:20 -0300

    boton-eliminar-ok

--- client/src/components/DetailEmpresa/DetailEmpresa.js
+++ client/src/components/DetailEmpresa/DetailEmpresa.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {useParams} from 'react-router-dom'
-import {getEmpresa, deleteEmpresa} from '../../redux/actions/empresaActions';
+
+import {getEmpresa} from '../../redux/actions/empresaActions';
 import styles from './styles.module.css'
 import Header from '../Header/Header';
 import data from './data.json';
