a3ba549ded8ffd299c682077f8024dd8ba428dec
Juliana Gonzalez <juliana.mg3@gmail.com>
2021-06-19 04:23:46 -0300

    ok

--- api/src/app.js
+++ api/src/app.js
@@ -9,59 +9,54 @@ require("./db.js");
 
 //=====passport ====
 const passport = require("./passport");
-const session = require('express-session')
+const session = require("express-session");
 
 //===================================================================
 
 server.use(cors()); //{ origin: process.env.REACT_APP_FRONT, credentials: true }
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); 
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
-	res.header('Access-Control-Allow-Credentials', 'true');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-	);
-	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-	next();
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
 });
-
-
 
 //=============================== middleware passport =========================
 
 server.use(
-	session({
-	  secret: "esto funciona perfect",
-	  resave: false,
-	  saveUninitialized: true,
-	})
+  session({
+    secret: "esto funciona perfect",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
 server.use(passport.initialize());
 server.use(passport.session());
-server.use(function(req, res, next){
-	res.locals.user = req.user || null
-	next();
-})
-  
+server.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 server.all("*", function (req, res, next) {
-	passport.authenticate("bearer", function (err, user) {
-    	// console.log("ACA ESTA EL UNDEFINED >> req user", req.user)
-		if (err) return res.status(400).json({ message: 'malformed JSON' });
-		if (user) {
-			req.user = user;
-		}
-		return next();
-	})(req, res, next);
+  passport.authenticate("bearer", function (err, user) {
+    // console.log("ACA ESTA EL UNDEFINED >> req user", req.user)
+    if (err) return res.status(400).json({ message: "malformed JSON" });
+    if (user) {
+      req.user = user;
+    }
+    return next();
+  })(req, res, next);
 });
 //===================================================================
-
-
 
 server.use("/", routes);
 
@@ -69,7 +64,7 @@ server.use("/", routes);
 server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
-  console.error("soy el error en app.js",err);
+  console.error("soy el error en app.js", err);
   res.status(status).send(message);
 });
 
