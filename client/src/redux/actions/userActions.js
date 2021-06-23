f233525f02a2a70c49f5e96f4e6690be04d5802f
julianactrl <juliana.mg3@gmail.com>
2021-06-22 22:24:50 -0300

    deploy

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -29,7 +29,7 @@ export const register = (body) => async (dispatch) => {
       headers: { "Content-Type": "application/json" },
     };
     const {data}  = await axios.post(
-      `${REACT_APP_API}/auth/api/signup`,
+      `${ REACT_APP_API_HEROKU}/auth/api/signup`,
       body,
       config
     );
@@ -61,7 +61,7 @@ export const login = ({email, password}) => async (dispatch) => {
     //   headers: { "Content-Type": "application/json" },
     // };
     const  data  = await axios.post(
-      `${REACT_APP_API}/auth/api/signin`,
+      `${ REACT_APP_API_HEROKU}/auth/api/signin`,
       { email, password },
       // config,
 	  
@@ -117,7 +117,7 @@ export const logout = () => {
 export const getUser = () => {
   return function(dispatch) {
     dispatch({type: LOADING_USER})
-    return axios.get(`${REACT_APP_API}/auth/myProfile` , BEARER())
+    return axios.get(`${ REACT_APP_API_HEROKU}/auth/myProfile` , BEARER())
     .then(async userInfo => {
       if (userInfo.data.jwt) localStorage.setItem('jwt', JSON.stringify(userInfo.data.jwt));
       delete userInfo.data.jwt
@@ -150,7 +150,7 @@ export const updateUser = ({id,fullName,email,password,profile_pic}) => {
     dispatch({ type: UPDATE_USER,payload: {fullName,email,password,profile_pic} });
     axios({
         method: 'put',
-        url: `${REACT_APP_API}/user/edit/${id}`,
+        url: `${ REACT_APP_API_HEROKU}/user/edit/${id}`,
         data: {
             fullName,
             email,
