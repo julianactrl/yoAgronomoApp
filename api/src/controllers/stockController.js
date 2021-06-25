fd350ee5840c07f1d14f8ecf8f48759e188591dd
Dario Rodriguez <69946404+Dariorod1@users.noreply.github.com>
2021-06-24 22:27:59 -0300

    routes de stock

--- api/src/controllers/stockController.js
+++ api/src/controllers/stockController.js
@@ -1,16 +1,18 @@
-const {Stock} = require("../db");
+const {Stock, Lote} = require("../db");
 const { Op } = require("sequelize");
 
 const postStock = async(req, res) => {
-    const {tipo, cantidad} = req.body;
+    const {tipo, cantidad,loteId} = req.body;
     try{
         await Stock.create({
             tipo,
-            cantidad
+            cantidad,
+            loteId
         });
         res.json('Stock generado con éxito!');
     }
     catch(error){
+        console.log(error)
         res.status(500).send('Error al crear el stock');
     }
 }
@@ -38,8 +40,47 @@ const getAllStock = async (req, res) => {
     const allStock = await Stock.findAll()
     res.send(allStock);
 }
+const getStockByLote = async (req,res) => {
+    const {id} = req.params
+    try {
+        const stock = await Stock.count();
+        if (stock !== 0) {
+          res.status(201).json(await Stock.findAll({
+              include: {
+                  model: Lote,
+                  where :{
+                      id
+                  }
+              }
+          }));
+        }
+      } catch (e) {
+        res.status(404).send(next);
+      }
+}
+const deleteStock = async(req,res) => {
+    const {id} = req.params
+    try {
+        const deletedStock = await Stock.destroy({
+            where: {
+                id
+            }
+        });
+        res.json({
+            message: "Stock deleted succesfully",
+            data: deletedStock
+        })
+    } catch (error) {
+        res.json({
+            message: "Delete failed",
+            data: {}
+        })
+    }
+}
 module.exports = {
     postStock,
     editStock,
-    getAllStock
+    getAllStock,
+    getStockByLote,
+    deleteStock
 }
