1a5e7548df2e267354dd1a2910c83a665826aca3
Dario Rodriguez <69946404+Dariorod1@users.noreply.github.com>
2021-06-26 15:29:27 -0300

    cambios

--- api/src/db.js
+++ api/src/db.js
@@ -82,7 +82,7 @@ User.belongsToMany(Role, {
   through: "user_role",
   foreignKey: "user_id",
 });
-Stock.belongsTo(Empresa);
+Stock.belongsTo(Empresa);//Sotck-Empresa
 Empresa.hasMany(Tarea);
 Tarea.belongsTo(Empresa);
 
