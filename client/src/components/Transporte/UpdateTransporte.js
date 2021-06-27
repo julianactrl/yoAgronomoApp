a79ec8aa3f8570ca7fffbcc9b53a71df6972c999
Dario Rodriguez <69946404+Dariorod1@users.noreply.github.com>
2021-06-27 13:20:51 -0300

    update transporte

--- client/src/components/Transporte/UpdateTransporte.js
+++ client/src/components/Transporte/UpdateTransporte.js
@@ -7,6 +7,7 @@ import Header from '../Header/Header';
 import { useHistory } from 'react-router';
 
 function UpdateTransporte ({id}) {
+    console.log("este es el id",id)
     const dispatch = useDispatch();
     const transporte = useSelector(state=>state.transporteReducer.transporteForId);
     const [input, setInput] = useState({
@@ -38,7 +39,7 @@ useEffect(()=> {
 const history = useHistory()
 function handleSubmit(e) {
     e.preventDefault();
-    axios.put(`http://localhost:3001/transporte/update/${id}`, input)
+   axios.put(`http://localhost:3001/transporte/update/${id}`, input)
         .then(response => console.log(response.data)) 
         .catch(error  => console.log(error))
     e.target.reset();
