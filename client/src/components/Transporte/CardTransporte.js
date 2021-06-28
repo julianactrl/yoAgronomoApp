4a6afd2c43307ee7287ef23e877e5df29cfe07f2
Felipeaescalante <faferg86@gmail.com>
2021-06-28 10:48:13 -0300

    css transporte

--- client/src/components/Transporte/CardTransporte.js
+++ client/src/components/Transporte/CardTransporte.js
@@ -37,12 +37,12 @@ function DetailTransporte (props) {
            {transporte && transporte.map(transport=>(
              
                <div className={styles.transporte}>
-                   <h3 >Patente: {transport.patente}</h3>
-                   <h3>Conductor: {transport.conductor}</h3>
-                   <h3>Carga: {transport.carga}</h3>
-                   <h3>Fecha de entrada: {transport.fechaEntrada}</h3>
-                   <h3>Fecha de salida: {transport.fechaSalida}</h3>
-                   <h3>Observaciones: {transport.observaciones}</h3>
+                   <h3 className={styles.items} >Patente: {transport.patente}</h3>
+                   <h3 className={styles.items} >Conductor: {transport.conductor}</h3>
+                   <h3 className={styles.items} >Carga: {transport.carga}</h3>
+                   <h3 className={styles.items} >Fecha de entrada: {transport.fechaEntrada}</h3>
+                   <h3 className={styles.items} >Fecha de salida: {transport.fechaSalida}</h3>
+                   <h3 className={styles.items} >Observaciones: {transport.observaciones}</h3>
                    <Link className={styles.editar} to={`/updatetransporte/${transport.id}`}>
                        <FontAwesomeIcon icon={faEdit}/>
                     </Link>
