62f73dc240855f71476189679737c05d460b0851
gonzalo12348 <gonzaloarancibia48@gmail.com>
2021-06-30 13:03:49 -0300

    Gonzalo Gastos Back : Terminado

--- api/src/routes/gastosRouter.js
+++ api/src/routes/gastosRouter.js
@@ -8,10 +8,13 @@ const {
     createClasificacion,
     deleteClasificacion,
     ///// GASTOS //////
+    getGastosByInput,
     createGasto,
     deleteGasto,
     updateGasto,
-    getAllGastos
+    getAllGastos,
+    ///// TOTAL /////
+    getTotal
 } = require("../controllers/gastosController");
 
 /////////////////////   CLASIFICACIONES   ///////////////////////////////
@@ -21,10 +24,14 @@ server.get("/clasificacion/:id", getAllClasificiones)
 server.delete("/deleteClasificacion/:id", deleteClasificacion)
 
 /////////////// GASTOS  /////////////////////////////////
-
+server.get('/getGastoByInput/:input', getGastosByInput)
 server.get('/getGasto/:id', getAllGastos)
 server.post("/createGasto", createGasto)
 server.delete('/deleteGasto/:id', deleteGasto)
 server.put("/updateGasto/:id",updateGasto)
+
+///////////////// TOTAL ///////////////
+
+server.get('/getTotal/:empresaId', getTotal)
 
 module.exports = server;
